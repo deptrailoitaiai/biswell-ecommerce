bb91dcbca2e3201669f2f4a3c5eac911037a1600
deptrailoitaiai <minh0912873465@gmail.com>
2024-12-23 18:25:50 +0700

    dhfood

--- src/main/java/org/example/controllers/renderControllers/HomePageController.java
+++ src/main/java/org/example/controllers/renderControllers/HomePageController.java
@@ -1,14 +1,14 @@
 package org.example.controllers.renderControllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
-@RequestMapping("/index")
+@Controller()
 public class HomePageController {
-    @GetMapping()
+    @GetMapping("/index")
     public String getHomePage(Model model) {
         return "index.html";
     }
