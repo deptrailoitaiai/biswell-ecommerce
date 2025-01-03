e58b3b786682236f219bb99e6f71ad238b3901c3
deptrailoitaiai <minh0912873465@gmail.com>
2025-01-03 16:24:43 +0700

    fix thymeleaf

--- src/main/java/org/example/controllers/renderControllers/HomePageController.java
+++ src/main/java/org/example/controllers/renderControllers/HomePageController.java
@@ -3,13 +3,11 @@ package org.example.controllers.renderControllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller()
 public class HomePageController {
     @GetMapping("/index")
     public String getHomePage(Model model) {
-        return "index.html";
+        return "index";
     }
 }
