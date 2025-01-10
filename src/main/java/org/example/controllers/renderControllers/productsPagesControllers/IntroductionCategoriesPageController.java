1911eb682fe2815e6448ea11110c28e989a9d3b0
deptrailoitaiai <minh0912873465@gmail.com>
2025-01-11 01:28:55 +0700

    fix color

--- src/main/java/org/example/controllers/renderControllers/productsPagesControllers/IntroductionCategoriesPageController.java
+++ src/main/java/org/example/controllers/renderControllers/productsPagesControllers/IntroductionCategoriesPageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller()
 public class IntroductionCategoriesPageController {
-    @GetMapping()
+    @GetMapping("/categories")
     public String getIntroductionCategoryPage(Model model) {
         return "categories";
     }
--- src/main/java/org/example/controllers/renderControllers/productsPagesControllers/category1Controllers/itemsControllers/Item1Controller.java
+++ src/main/java/org/example/controllers/renderControllers/productsPagesControllers/category1Controllers/itemsControllers/Item1Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller()
 public class Item1Controller {
-    @GetMapping("/item1")
+    @GetMapping("/productOfCategory1")
     public String getItem1Page(Model model) {
         return "productOfCategory1";
     }
