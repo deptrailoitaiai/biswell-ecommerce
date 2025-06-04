f82740e84f1039ed0f3cbb211c139f2d60697f8c
K4T <tuanka197st@gmail.com>
2025-06-04 10:37:55 +0700

    Update code 4/6

--- src/main/java/org/example/entities/ProductEntity.java
+++ src/main/java/org/example/entities/ProductEntity.java
@@ -2,17 +2,7 @@ package org.example.entities;
 
 import java.util.List;
 
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,7 +19,7 @@ public class ProductEntity {
     private Long id;
     @Column
     private String pname;
-    @Column
+    @Column(columnDefinition = "TEXT")
     private String pdesc;
 //    @Column
 //    private String
